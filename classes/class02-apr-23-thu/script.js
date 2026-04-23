b1fc463cf45dfca2ba949b17cdf3ce340b91b9a7
Tony Yu <sugonyu@gmail.com>
2026-04-23 14:07:23 -0400

    DOM Test

--- classes/class02-apr-23-thu/script.js
+++ classes/class02-apr-23-thu/script.js
@@ -1,1 +1,29 @@
 // alert(1)
+const pr = console.log;
+
+//--------------------------------------
+// pr(1);
+
+// // pr(2)
+// setTimeout(() => pr(2), 1000);
+
+// pr(3);
+
+
+//---------------- Callback function ----------------------------
+// ❌ 함정! 모든 선배가 같은 사물함을 씀
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => console.log(i), 1000);
+// }
+
+
+// ✅ 해결! let 쓰면 각자 다른 사물함
+for (let i = 0; i < 3; i++) {
+  setTimeout(() => console.log(i), 1000);
+}
+
+
+//------------------ DOM -----------------------
+
+const title = document.getElementById("bar")
+title.textContent = "bar"
